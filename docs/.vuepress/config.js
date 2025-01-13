76fc2208e3aabbb649fca444a3fed86651969156
clarence <1033627154@qq.com>
2025-01-13 11:54:15 +0800

    提交

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,6 @@ import { defineUserConfig } from 'vuepress'
 import { defaultTheme } from '@vuepress/theme-default'
 
 export default defineUserConfig({
-  base: '/website/',  // 设置为你的仓库名称
   lang: 'zh-CN',
   title: 'My VuePress Site',
   description: 'Just playing around',
